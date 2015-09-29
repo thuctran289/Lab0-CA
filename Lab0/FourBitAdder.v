5f19dd5b1a92e1b28594e1457636b5e9bf2871da
Ziyilan <ziyi.lan@students.olin.edu>
2015-09-29 15:52:14 -0400

    Added Testbench

--- Lab0/FourBitAdder.v
+++ Lab0/FourBitAdder.v
@@ -4,49 +4,66 @@
 `define NOT not #50
 `define XOR xor #50
 
-module FullAdder4bit
-	(
-		output [3:0] sum, 
-		output carryout, 
-		output overflow,
-		input[3:0] a,
-		input[3:0] b
-	);
+module FourBitFullAdder(out, cout, a, b, carryin);
 
-	// four 1-bit adders chained together to make a 4-bit adder
-	wire c0, c1,c2;
-	structuralFullAdder adder0 (sum[0], c0, a[0], b[0], 0);
-	structuralFullAdder adder1 (sum[1], c1, a[1], b[1], c0);
-	structuralFullAdder adder2 (sum[2], c2, a[2], b[2], c1);
-	structuralFullAdder adder3 (sum[3], carryout, a[3], b[3], c2);
+output [3:0]out;
+output cout;
+input [3:0] a;
+input [3:0] b;
+input carryin;
+wire c0, c1,c2;
+structuralFullAdder adder0 (out[0], c0, a[0], b[0], carryin);
+structuralFullAdder adder1 (out[1], c1, a[1], b[1], c0);
+structuralFullAdder adder2 (out[2], c2, a[2], b[2], c1);
+structuralFullAdder adder3 (out[3], cout, a[3], b[3], c2);
 
-	// XOR the carry in and carryout of the 4th adder to check for overflow
-	`XOR(overflow, c2, carryout);
 endmodule
 
 module test4BitAdd;
-	wire [3:0]out;
-	wire cout;
-	reg [3:0] a;
-	reg [3:0] b;
-	reg carryin;
-	FullAdder4bit add(out, cout,overflow, a,b);
+wire [3:0]out;
+wire cout;
+reg [3:0] a;
+reg [3:0] b;
+reg carryin;
+FourBitFullAdder add(out, cout, a,b,carryin);
 
-	initial begin
-		a = 5; b = 9;  #1000
-		$display(" %b   %b   %b |   %b      |    0        0", b, a, out, cout);
-		a = 5; b = 15;  #1000
-		$display(" %b   %b   %b |   %b      |    0        0", b, a, out, cout);
-		a = 1; b = 15;  #1000
-		$display(" %b   %b   %b |   %b      |    0        0", b, a, out, cout);
-		a = 0; b = 15;  #1000
-		$display(" %b   %b   %b |   %b      |    0        0",  b, a, out, cout);
-		a = 15; b = 0;  #1000
-		$display(" %b   %b   %b |   %b      |    0        0",  b, a, out, cout);
-		a = 15; b = 15; #1000
-		$display(" %b   %b   %b |   %b      |    0        0",  b, a, out, cout);
-
-	end
+initial begin
+$display(" a   b   carryin |   Sum       Carryout | Expected sum   Expected Carryout|", a, b, carryin,out, cout);
+a = 1111; b = 0000; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1111                  0      |", a, b, carryin,out, cout);
+a = 1001; b = 0001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1010                  0      |", a, b, carryin,out, cout);
+a = 1011; b = 0001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1100                  0      |", a, b, carryin,out, cout);
+a = 1111; b = 0001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0000                  1      |", a, b, carryin,out, cout);
+a = 1111; b = 0000; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1111                  0      |", a, b, carryin,out, cout);
+a = 1111; b = 1111; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1110                  1      |", a, b, carryin,out, cout);
+a = 1001; b = 0110; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1111                  0      |", a, b, carryin,out, cout);
+a = 1010; b = 1011; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1101                  0      |", a, b, carryin,out, cout);
+a = 0111; b = 0001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1000                  0      |", a, b, carryin,out, cout);
+a = 1000; b = 1000; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0000                  1      |", a, b, carryin,out, cout);
+a = 1010; b = 1010; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0100                  1      |", a, b, carryin,out, cout);
+a = 0111; b = 1110; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0101                  1      |", a, b, carryin,out, cout);
+a = 0001; b = 0001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0010                  0      |", a, b, carryin,out, cout);
+a = 0010; b = 0010; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0100                  0      |", a, b, carryin,out, cout);
+a = 0100; b = 0100; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1000                  0      |", a, b, carryin,out, cout);
+a = 1001; b = 1001; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    0010                  1      |", a, b, carryin,out, cout);
+a = 0101; b = 0101; carryin = 0; #1000
+$display(" %b   %b   %b    |   %b            %b   |    1010                  0      |", a, b, carryin,out, cout);
+end
 endmodule
 
 
