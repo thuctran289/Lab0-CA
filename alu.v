c5ba7724aa01e343f32c3da1afe5f6074025acd1
Jordan Van Duyne <jordannn808@gmail.com>
2015-10-14 23:16:59 -0400

    finish alu

--- alu.v
+++ alu.v
@@ -14,24 +14,27 @@
 `define bAND and #50
 `define bOR or #90
 `define ANDtri and #40
+`define OR32 or #330
 
 // LUT to control the inputs of the ALU (the mux command and ~B)
 module ALUcontrolLUT (
         output reg[2:0]     muxindex,
         output reg  invertB,
-        input[2:0]  ALUcommand
+        input[2:0]  ALUcommand,
+        output reg[31:0] sltResult,
+        output reg sltSet
     );
 
     always @(ALUcommand) begin
         case (ALUcommand)
-            `ADDo:  begin muxindex = 0; invertB=0; end    
-            `SUBo:  begin muxindex = 0; invertB=1; end
-            `XORo:  begin muxindex = 1; invertB=0; end    
-            `SLTo:  begin muxindex = 0; invertB=1; end
-            `ANDo:  begin muxindex = 3; invertB=0; end    
-            `NANDo: begin muxindex = 4; invertB=0; end
-            `NORo:  begin muxindex = 5; invertB=0; end    
-            `ORo:   begin muxindex = 6; invertB=0; end
+            `ADDo:  begin muxindex = 0; invertB=0;  sltResult=0; sltSet=0; end    
+            `SUBo:  begin muxindex = 0; invertB=1;  sltResult=0; sltSet=0; end
+            `XORo:  begin muxindex = 1; invertB=0;  sltResult=0; sltSet=0; end    
+            `SLTo:  begin muxindex = 0; invertB=1;  sltResult=0; sltSet=1; end
+            `ANDo:  begin muxindex = 3; invertB=0;  sltResult=0; sltSet=0; end    
+            `NANDo: begin muxindex = 4; invertB=0;  sltResult=0; sltSet=0; end
+            `NORo:  begin muxindex = 5; invertB=0;  sltResult=0; sltSet=0; end    
+            `ORo:   begin muxindex = 6; invertB=0;  sltResult=0; sltSet=0; end
         endcase
     end
 endmodule
@@ -49,9 +52,13 @@ module ALU (
     wire bInv;
     wire [2:0] deviceChoice;
     wire [32:0] carry;
+    wire [31:0] tempresult;
     wire notCommand2, notCommand1;
     wire tempOverflow;
-    ALUcontrolLUT aluControlLUT(deviceChoice,bInv,command);
+    wire tempZero;
+    wire [31:0] sltResult;
+    wire sltSameSign, sltOppTemp, sltOpp, notOverflow, sltChoice;
+    ALUcontrolLUT aluControlLUT(deviceChoice,bInv,command, sltResult, sltChoice);
 
     genvar i;
     generate
@@ -59,13 +66,13 @@ module ALU (
             if (i == 0)
                 begin
                     assign carry[i] = bInv;
-                    structuralALU strucALU(result[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice); 
+                    structuralALU strucALU(tempresult[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice); 
             end else if (i == 31) 
                 begin
-                    structuralALU sALU(result[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice); 
+                    structuralALU sALU(tempresult[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice); 
             end else 
                 begin
-                    structuralALU structureALU(result[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice);
+                    structuralALU structureALU(tempresult[i], carry[i+1], operandA[i],operandB[i], carry[i], bInv, deviceChoice);
             end
         end
     endgenerate
@@ -74,6 +81,21 @@ module ALU (
     `ANDtri (carryout, carry[32], notCommand1, notCommand2);
     ourXOR ourxor(tempOverflow, carry[31], carryout);
     `ANDtri (overflow, tempOverflow, notCommand1, notCommand2);
+
+    // SLT
+    `NOT(notOverflow, tempOverflow);
+    `AND(sltOppTemp, tempresult[31], overflow);
+    `AND(sltOpp,sltOppTemp, operandA[31]);
+    `AND(sltSameSign, tempresult[31], notOverflow);
+    `OR(sltResult[0], sltSameSign, sltOpp);
+
+    thirtyTwoMux mux32(result, tempresult, sltResult, sltChoice);
+
+    
+
+    `OR32(tempZero, result);
+    `NOT(zero, tempZero);
+
 
 endmodule
 
@@ -153,6 +175,23 @@ module eightToOneMux(input in0, input in1, input in2, input in3, input in4,input
     `bAND(temp7, nnaddr0, nnaddr1, nnaddr2, in7);
 
     `bOR(out, temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7);
+endmodule
+
+module thirtyTwoMux(output [31:0]out, input [31:0]in1, input [31:0]in2, input selector);
+    wire notSelector;
+    wire [31:0]andIn1;
+    wire [31:0]andIn2;
+
+    `NOT(notSelector, selector);
+    genvar i;
+    generate
+        for (i=0; i < 32; i=i+1) begin : ANDGenerator
+            `AND(andIn1[i], in1[i], notSelector);
+            `AND(andIn2[i], in2[i], selector);
+            `OR(out[i], andIn1[i], andIn2[i]);
+        end
+    endgenerate
+
 endmodule
 
 module testALU;
