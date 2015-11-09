4b8de41e92969cf90a8a54b5cb8878542769e7e8
Jordan Van Duyne <jordannn808@gmail.com>
2015-11-09 15:48:40 -0500

    clean up test bench

--- Lab2/inputconditioner.t.v
+++ Lab2/inputconditioner.t.v
@@ -25,16 +25,20 @@ module testConditioner();
         $dumpfile("inputconditioner.t.vcd");
         $dumpvars(0, testConditioner);
         $display("Input conditioner test bench");
+        $display("============================");
         // TEST BENCH IS FOR WAITTIME = 3
         pin = 0;
         #300
         pin = 1;
         i=0;
+        // There should be 5 cycles of 20 time units before the 
+        // input is counted as actually being high (not just a glitch)
         for (i=0; i<5; i=i+1) begin
             #20
             if (conditioned == 1)
                 $display("DUT inputconditioner failed! Conditioned went high too soon");
         end
+        // After the 5 cycles, conditioned should then be set high
         #20
         if (conditioned == 0) begin
             $display("DUT inputconditioner failed! Conditioned does not go high soon enough");
@@ -44,26 +48,31 @@ module testConditioner();
             $display("DUT inputconditioner passed!");
         end
         #300
+        // Set input back to 0
+        pin = 0;
+        #300
+        // Glitch; should not set conditioned
+        pin = 1;
+        #70
         pin = 0;
         #300
         pin = 1;
-        #250
+        #200 // 200 is longest for 10 waittime, 70 is longest time for glitch for 3 waittime
         pin = 0;
         #500
-        pin = 1;
-        #200 // 220 is longest for 10 waittime, 70 is longest time for glitch for 3 waittime
-        pin = 0;
-        #500
+        // These following glitches should not end up setting conditioned to high
         pin = 1;
         #20
         pin = 0;
         #20
         pin = 1;
+        #20
+        pin = 0;
+        #20
+        // should set conditioned to high
+        pin = 1;
         #300
         pin = 0;
-        #20 $finish;
-    // Your Test Code
-    // Be sure to test each of the three conditioner functions:
-    // Synchronize, Clean, Preprocess (edge finding)
+        #300 $finish;
     end
 endmodule
